14aa7d65d46e0fb4b63bdfaa25671fbb31f7e79f
enextus <enextus@gmail.com>
2022-06-19 10:44:20 +0200

    sd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,13 @@ package org.example;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+
+		CastToTypedMap castToTypedMap = new CastToTypedMap();
+
+		System.out.println(castToTypedMap.getClass());
+		System.out.println(castToTypedMap.getRawMap());
+		System.out.println(castToTypedMap.toString());
+
 	}
+
 }
